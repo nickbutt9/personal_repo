db5f6a58b9ca0568b7c34d4d6feced6291f6f95d
Nick Butt <nickb@adhawkmicrosystems.com>
2023-01-05 11:55:36 -0500

    Adding code from simulating neurobiological systems
    This commit contains the code from the 5 assignments assigned in UWs SYDE556 - simulating neurobiological systems course.

---
+++ school_projects/SYDE556_simulating_neurobiological_systems/__init__.py
@@ -0,0 +1,4 @@
+# This directory contains the coding assignments required in SYD556 - simulating neurobiological systems
+
+# The first three assignments focus on creating neuron models and functionality using Numpy, and the last two
+# use Nengo to create more complex neuro systems
